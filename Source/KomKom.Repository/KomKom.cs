76e3cd2300ef8efda12c3f51e3b1a334b2b24df8
maxosba <daffea@gmail.com>
2013-01-11 23:11:41 -0500

    c

---
+++ Source/KomKom.Repository/KomKom.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.Entity;
+using KomKom.Model.Breeding;
+using KomKom.Model.Farming;
+using KomKom.Model.Fishing;
+using KomKom.Model;
+
+
+namespace KomKom.Repository
+{
+    public class KomKom : DbContext
+    {
+        public DbSet<Farmer> Farmers { get; set; }
+        public DbSet<FarmingProductOffer> FarmingProductOffers { get; set; }
+        public DbSet<Fisherman> Fishermen { get; set; }
+        public DbSet<FishingProductOffer> FishingProductOffers { get; set; }
+        public DbSet<Breeder> Breeders { get; set; }
+        public DbSet<BreedingProductOffer> BreedingProductOffers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<MapLocator> MapLocators { get; set; }
+        public DbSet<Offer> Offers { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public KomKom(): base("DefaultConnection")
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<KomKom>());
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // inherited table types
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<Farmer>().ToTable("Users_Farmers");
+            modelBuilder.Entity<Breeder>().ToTable("Users_Breeders");
+            modelBuilder.Entity<Fisherman>().ToTable("Users_Fishermen");
+            modelBuilder.Entity<Offer>().ToTable("Offers");
+            modelBuilder.Entity<BreedingProductOffer>().ToTable("Offers_BreedingProductOffers");
+            modelBuilder.Entity<FarmingProductOffer>().ToTable("Offers_FarmingProductOffers");
+            modelBuilder.Entity<FishingProductOffer>().ToTable("Offers_FishingProductOffers");
+        }
+    }
+}
---
+++ Source/KomKom.Repository/RepositoryBase.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+
+namespace KomKom.Repository
+{
+    public class RepositoryBase<C> : IDisposable where C : DbContext, new()
+    {
+        private C _dataContext;
+
+        public virtual C DataContext
+        {
+            get
+            {
+                if (_dataContext == null)
+                {
+                    _dataContext = new C();
+                    this.AllowSerialization = true;
+                }
+                return _dataContext;
+            }
+        }
+        public virtual T Get<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            if (predicate != null)
+            {
+                using (DataContext)
+                {
+                    return DataContext.Set<T>().Where(predicate).SingleOrDefault();
+                }
+            }
+            else
+            {
+                throw new ApplicationException("Predicate value must be passed to Get<T>.");
+            }
+        }
+        public virtual IQueryable<T> GetList<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            try
+            {
+                return DataContext.Set<T>().Where(predicate);
+            }
+            catch (Exception)
+            {
+                //Log error
+            }
+            return null;
+        }
+        public virtual IQueryable<T> GetList<T, TKey>(Expression<Func<T, bool>> predicate,
+            Expression<Func<T, TKey>> orderBy) where T : class
+        {
+            try
+            {
+                return GetList(predicate).OrderBy(orderBy);
+            }
+            catch (Exception ex)
+            {
+                //Log error
+            }
+            return null;
+        }
+        public virtual IQueryable<T> GetList<T, TKey>(Expression<Func<T, TKey>> orderBy) where T : class
+        {
+            try
+            {
+                return GetList<T>().OrderBy(orderBy);
+            }
+            catch (Exception ex)
+            {
+                //Log error
+            }
+            return null;
+        }
+        public virtual IQueryable<T> GetList<T>() where T : class
+        {
+            try
+            {
+                return DataContext.Set<T>();
+            }
+            catch (Exception ex)
+            {
+                //Log error
+            }
+            return null;
+        }
+        public virtual bool AllowSerialization
+        {
+            get
+            {
+                return _dataContext.Configuration.ProxyCreationEnabled;
+            }
+            set
+            {
+                _dataContext.Configuration.ProxyCreationEnabled = !value;
+            }
+        }
+        public void Dispose()
+        {
+            if (DataContext != null) DataContext.Dispose();
+        }
+    }
+}
